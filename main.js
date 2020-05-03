507ca3c6672996aa6599eab8ad2159ae14f5ba81
NIK DJETELI <innodjet@gmail.com>
2020-05-03 17:01:08 -0500

    Quick logic change

--- main.js
+++ main.js
@@ -1,33 +1,54 @@
-var title = 'spiderman';
-var page = 1;
-var total = '';
-var total_pages = '';
-var title_data = [];
-var url = 'https://jsonmock.hackerrank.com/api/movies/search/';
+var https = require('https');
 
-fetch(url+'?Title='+title+'&page='+page)
-.then(  res  => res.json()) 
-.then(  data => { total_pages = data.total_pages, 
-                  total = data.total,
-                  // Push the first page titles to the array,
-                  data.data.forEach(element => {
-                      title_data.push(element.Title) 
-                  });
-                  // Next if total pages > 1 then we do next loop 
-                  // to fetch the data up the end of the pages
-                  for (let i = 2; i <= total_pages; i++) {
-                    newFetch(title , i);
-                  };
-                  console.log(title_data);
-      })
-.catch(error => console.error(error));
+const getMovieTitles = (substr) => {
+   let title = substr , page = 1 , total = '';
+   let total_pages, title_data = [] ,  url = 'https://jsonmock.hackerrank.com/api/movies/search/';
+   https.get(url+'?Title='+title+'&page='+page, (res) => {
+    res.on('data', (body) => {
+        let data = JSON.parse(body);
+        total_pages = data.total_pages,
+        total = data.total,
+        // push the first page titles to the array
+        data.data.forEach(  el => {
+            title_data.push(el.Title)
+        });
+        title_data.sort();
+        // Next if total pages > 1 then we do next loop
+        // to fetch the data up to the end of the pages
+        if (total_pages > 1) {
+          for (let i = 2; i <= total_pages; i++) {
+            newFetch(title,i);
+          }
+        } else {
+          console.log('...................');
+          console.log(title_data.join('\n'));
+          console.log('...................');
+        }
+        
+    });
 
-const newFetch = async (fetchTitle , newPage) => {
-  const rep = await fetch(url+'?Title='+fetchTitle+'&page='+newPage);
-  const data = await rep.json();
-  // push title to the array,
-  data.data.forEach(element => {
-      title_data.push(element.Title) 
-  });
-  title_data.sort();
+    }).on('error', (e) => {
+        console.error(e);
+    });
+
+
+    const newFetch = (fetchTitle, newPage) => {
+        https.get(url+'?Title='+fetchTitle+'&page='+newPage, (res) => {
+            res.on('data', (body) => {
+                let data = JSON.parse(body);
+                // push title to the array
+                data.data.forEach( el => {
+                    title_data.push(el.Title);
+                });
+                title_data.sort();
+                console.log('...................');
+                console.log(title_data.join('\n'));
+                console.log('...................');
+            });
+            }).on('error', (e) => {
+                console.error(e);
+            });
+    }
 }
+
+getMovieTitles('spiderman');
