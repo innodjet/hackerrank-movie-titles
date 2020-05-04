81b7b327cfb8c186ffd73dc64574caebcc44d465
NIK DJETELI <innodjet@gmail.com>
2020-05-04 02:39:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,46 +7,45 @@ const getMovieTitles = (substr) => {
   
   https.get(url+'?Title='+title+'&page='+page, (res) => {
     res.on('data', (body) => {
-        let data = JSON.parse(body);
-        total_pages = data.total_pages;
-        // push the first page titles to the array
-        data.data.forEach(  el => {
-            title_data.push(el.Title);
-        });
-        title_data.sort();
-        // Next if total pages > 1 then we do next loop
-        // to fetch the data up to the end of the pages
-        if (total_pages > 1) {
-          for (let i = 2; i <= total_pages; i++) {
-            newFetch(title,i);
-          }
-        } else {
-          console.log('...................');
-          console.log(title_data.join('\n'));
-          console.log('...................');
+      let data = JSON.parse(body);
+      total_pages = data.total_pages;
+      // push the first page titles to the array
+      data.data.forEach(  el => {
+          title_data.push(el.Title);
+      });
+      title_data.sort();
+      // Next if total pages > 1 then we do next loop
+      // to fetch the data up to the end of the pages
+      if (total_pages > 1) {
+        for (let i = 2; i <= total_pages; i++) {
+          newFetch(title,i);
         }
-        
+      } else {
+        console.log('...................');
+        console.log(title_data.join('\n'));
+        console.log('...................');
+      }   
     });
   }).on('error', (e) => {
       console.error(e);
   });
 
   const newFetch = (fetchTitle, newPage) => {
-      https.get(url+'?Title='+fetchTitle+'&page='+newPage, (res) => {
-          res.on('data', (body) => {
-              let data = JSON.parse(body);
-              // push title to the array
-              data.data.forEach( el => {
-                  title_data.push(el.Title);
-              });
-              title_data.sort();
-              console.log('...................');
-              console.log(title_data.join('\n'));
-              console.log('...................');
+    https.get(url+'?Title='+fetchTitle+'&page='+newPage, (res) => {
+        res.on('data', (body) => {
+          let data = JSON.parse(body);
+          // push title to the array
+          data.data.forEach( el => {
+              title_data.push(el.Title);
           });
-      }).on('error', (e) => {
-          console.error(e);
-      });
+          title_data.sort();
+          console.log('...................');
+          console.log(title_data.join('\n'));
+          console.log('...................');
+        });
+    }).on('error', (e) => {
+      console.error(e);
+    });
   }
 }
 
